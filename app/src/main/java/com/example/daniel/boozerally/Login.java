68c4fc6cea2f4a0597e5b06b1285ea9258991442
Mihai <mihalimihai97@gmail.com>
2018-11-24 17:03:10 +0200

    list view

--- app/src/main/java/com/example/daniel/boozerally/Login.java
+++ app/src/main/java/com/example/daniel/boozerally/Login.java
@@ -2,12 +2,28 @@ package com.example.daniel.boozerally;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
-public class Login extends AppCompatActivity {
-
+public class Login extends AppCompatActivity  {
+        private Button mBtn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mBtn1 = (Button) findViewById(R.id.harti);
+        mBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHartile();
+            }
+        });
+    }
+
+    private void openHartile() {
+        Intent intent = new Intent(this, hartile.class);
+        startActivity(intent);
     }
 }
